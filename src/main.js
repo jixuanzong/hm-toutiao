c3c4400ff9e867056ed9fe828df4780de81cd256
JiXuanZong <jixuanzong@qq.com>
2019-12-23 17:16:32 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 日志的详细与否，生产环境，开发环境
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+// main.js 入口文件
+// 职责：做全局范围内的依赖和配置
+// 职责：创建一个根VUE实例
